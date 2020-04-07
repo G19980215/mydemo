ffd295ecd5a3e413d6bbc307acb1924d5d4a1844
ghj <179891800@qq.com>
2020-04-07 13:04:15 +0800

    my project

---
+++ ssm/src/main/java/com/zb/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.zb.service.impl;
+
+import com.zb.entity.User;
+import com.zb.mapper.UserMapper;
+import com.zb.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserMapper userMapper;
+    @Override
+    public User findUserLogin(String uname, String upwd) {
+        return userMapper.findUserLogin(uname,upwd);
+    }
+}
---
+++ ssm/target/ssm/main.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2020/3/25 0025
+  Time: 18:44
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+欢迎${sys_user.nickname}登录成功
+</body>
+</html>
